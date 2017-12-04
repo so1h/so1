db74c820dfc9af7a4cc06473a2adc8f28c5100e1
Pedro Pablo Lopez Rodriguez <plopez@etsisi.upm.es>
2017-12-05 00:59:23 +0100

    PRACT0/_MBR/TPART.C se ha modificado corrigiendo la primera entrada de la tabla de particiones de manera que el numero se pueda deducir de ella que la geometria del disco es de 63 sectores por pista, 16 cabezas y los cilindros que corresponda para 32 MB.
    PRACT0/_PBRFAT16/_BPBFAT16/BPBFAT16.H se modifica para ajustarse a la
    nueva geometría del disco.

--- PRACT0/_MBR/TPART.C
+++ PRACT0/_MBR/TPART.C
@@ -4,9 +4,6 @@
 /*   Tabla de particiones conteniendo una particion activa de tipo FAT16   */
 /* ----------------------------------------------------------------------- */
 
-//#include <so1pub.h\tipos.h>            /* byte_t, word_t, dword_t, FALSE */
-//#include <_mbr\mbr.h>                                 /* descParticion_t */
-
 #include <so1pub.h\tipos.h>              /* byte_t, word_t, dword_t, FALSE */
 #include <_mbr\mbr.h>                                   /* descParticion_t */
 
@@ -33,32 +30,29 @@ typedef struct                                          /* descParticion_t */
 /*  |     |  |  |  0x04 tipo de la particion (FAT16), 0x00 p. vacia        */
 /*  |     |  |  |  |      H,  S,  C finales de la particion                */
 /*  |     |  |  |  |      |   |   |  63 primer sector logico de la p.      */
-/*  |     |  |  |  |      |   |   |   |  65536 numero de sectores de la p. */
+/*  |     |  |  |  |      |   |   |   |  65457 numero de sectores de la p. */
 
 descParticion_t descParticion [ maxParticiones ] =
 {
-    { 0x80, 1, 1, 0, 0x04, 20, 16,  4, 63L, 65473L },         /*  16 bytes */
+    { 0x80, 1, 1, 0, 0x04, 15, 63, 64, 63L, 65457L },         /*  16 bytes */
     { 0x00, 0, 0, 0, 0x00,  0,  0,  0,  0L,     0L },         /*  16 bytes */
     { 0x00, 0, 0, 0, 0x00,  0,  0,  0,  0L,     0L },         /*  16 bytes */
     { 0x00, 0, 0, 0, 0x00,  0,  0,  0,  0L,     0L }          /*  16 bytes */
 } ;                                                           /*  64 bytes */
 
 /* Particion mas grande que cabe en un disco de 32 MB con geometria:       */
-/* (C, H, S, B) = (5+, 255, 63, 512)                                       */
+/* (C, H, S, B) = (C, 16, 63, 512)                                         */
 /* Primera particion: 0                                                    */
 /* Sector 0 ==> MBR                                                        */
 /* Primer sector de la particion 0 ==> Primer sector de la pista 1         */
-/* (C, H, S) = (0, 1, 1) primer sector logico = 0*255*63+1*63+(1-1) = 63   */
+/* (C, H, S) = (0, 1, 1) primer sector logico = 0*16*63+1*63+(1-1) = 63    */
 /* 32MB = 32*1024*1024 Bytes = 64*1024 sectores = 65536 sectores           */
 /* Ultimo sector de la particion 0 ==> 65536-1 = 65535                     */
-/* C = 65535/(255*63) = 65535/16065 = 4,                                   */
-/*     resto = 65535-(4*16065) = 65535-64260 = 1275                        */
-/* H = 1275/63 = 20                                                        */
-/*     resto = 1275-20*63 = 1275-1260 = 15                                 */
+/* C = 65535/(16*63) = 65535/1008 = 65,                                    */
+/*     resto = 65535-(65*1008) = 65535-65520 = 15                          */
+/* H = 15/63 = 0                                                           */
+/*     resto = 15-0*63 = 15-0 = 15                                         */
 /* S = 15+1 = 16                                                           */
-/* (C, H, S) = (4, 20, 16)                                                 */
-/* Numero de sectores en la particion = 65535-63+1 = 65473                 */
-
-
-
+/* (C, H, S) = (65, 0, 16)                                                 */
+/* Numero de sectores en la particion = 65536-63 = 65473                   */
 
